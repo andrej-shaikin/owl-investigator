219d3cd6b894ecbe4e2154d1002a8232aa662da7
andrejsajkin <kiwibon@yandex.ru>
2023-10-03 15:22:44 +0300

    Init

---
+++ src/main/kotlin/ru/shaikin/rebornomega/RebornOmegaApplication.kt
@@ -0,0 +1,11 @@
+package ru.shaikin.rebornomega
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RebornOmegaApplication
+
+fun main(args: Array<String>) {
+    runApplication<RebornOmegaApplication>(*args)
+}
